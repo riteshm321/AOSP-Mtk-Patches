5c9864d860273afdae6eb221e96b7c52433dd29e
Ritzz <33751927+riteshm321@users.noreply.github.com>
2018-05-19 17:41:50 +0530

    Update apply-patches.sh

--- apply-patches.sh
+++ apply-patches.sh
@@ -1,33 +1,12 @@
-#!/bin/bash
-cd ../../../..
-cd system/core
-git apply -v ../../device/Coolpad/CP8676_I02/patches/system_core.patch
-cd ../..
-cd bionic
-git apply -v ../device/Coolpad/CP8676_I02/patches/bionic.patch
-cd ..
-cd system/sepolicy
-git apply -v ../../device/Coolpad/CP8676_I02/patches/system_sepolicy.patch
-cd ../..
-cd packages/apps/Settings
-git apply -v ../../../device/Coolpad/CP8676_I02/patches/settings.patch
-cd ../../..
-cd frameworks/av
-git apply -v ../../device/Coolpad/CP8676_I02/patches/frameworks_av.patch
-cd ..
-cd native
-git apply -v ../../device/Coolpad/CP8676_I02/patches/frameworks_native.patch
-cd ..
-cd base
-git apply -v ../../device/Coolpad/CP8676_I02/patches/frameworks_base.patch
-cd ..
-cd opt/telephony
-git apply -v ../../../device/Coolpad/CP8676_I02/patches/frameworks_opt_telephony.patch
-cd ../../..
-cd packages/apps/FMRadio
-git apply -v ../../../device/Coolpad/CP8676_I02/patches/fmradio.patch
-cd ../../..
-cd system/netd
-git apply -v ../../device/Coolpad/CP8676_I02/patches/system_netd.patch
-cd ../..
-
+echo $1
+rootdirectory="$PWD"
+dirs="bionic fmradio frameworks/av frameworks/base frameworks/native frameworks/opt/telephony system/core system/netd system/sepolicy packages/apps/settings"
+for dir in $dirs ; do
+	cd $rootdirectory
+	cd $dir
+	echo "Applying $dir patches..."
+	git apply $rootdirectory/device/coolpad/note3/patches/$dir/*.patch
+	echo " "
+done
+echo "Changing to build directory..."
+cd $rootdirectory
